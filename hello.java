d46f2de725b134506b76e5b3a93f3b5ac8aa4df5
ms-shakil <mdshakil8796@gmail.com>
2022-06-24 20:10:26 +0600

    first java code

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void main(String[] args) {
+        int n =50;
+        for(int i =1;i<=n;i++){
+            if(i %2 == 0){
+            System.out.println(i);
+    
+            }
+        }
+    
+    }
+}
