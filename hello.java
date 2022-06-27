abfafd73fa0bb986fd58681a8a3434bbe578af61
ms-shakil <mdshakil8796@gmail.com>
2022-06-27 15:19:45 +0600

    spl lab problem

--- hello.java
+++ hello.java
@@ -10,3 +10,18 @@ public class hello {
     
     }
 }
+
+public class hello {
+    public static void main(String[] args) {
+        int n=0,i =0,k=1;
+        while(i <=56){
+                     
+            System.out.println(i);
+            n =i;
+            i+=k;
+            k=n;
+
+        }
+    
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+            // if(i ==2){
+            //     System.out.println(1); 
+
+            //   } 
