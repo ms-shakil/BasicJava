750bb80a68627193e3b1b6386ffee872df797de0
ms-shakil <mdshakil8796@gmail.com>
2022-06-29 00:07:52 +0600

    user input , array and array sort

--- array.java
+++ array.java
@@ -12,9 +12,23 @@ public class array {
             arr[i] =sc.nextInt();
         }
         for(int i =0;i<arr.length;i++){
-            System.out.println(arr[i]);
+            for(int j =i;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp =arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
             
         }
-        
+
+        int len  = arr.length;
+        int Hg = arr[len-1];
+        for(int i = len -1;-1<i;i--){
+            if(Hg>arr[i]){
+            System.out.println("2 nd height value is ="+arr[i]);
+            break;
+            }
+        }
     }
 }
